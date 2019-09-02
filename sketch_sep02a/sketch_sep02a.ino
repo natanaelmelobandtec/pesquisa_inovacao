73ec3ef606d835c2fcb54b420c4aba559235c88a
matheuscostabandtec <matheus.costa@bandtec.com.br>
2019-09-02 13:54:49 -0300

    Atualizaçaõ, simulador e arduino

---
+++ sketch_sep02a/sketch_sep02a.ino
@@ -0,0 +1,23 @@
+#include <dht.h>
+
+dht DHT;
+uint32_t timer = 0;
+void setup() 
+{
+  Serial.begin(9600);
+}
+void loop() {
+  if(millis() - timer >=2000)
+{
+
+DHT.read11(A1);
+Serial.print(DHT.humidity);
+Serial.println(" %");
+
+Serial.print(DHT.temperature);
+Serial.println(" Celsius");
+
+timer = millis();
+}
+}
+
